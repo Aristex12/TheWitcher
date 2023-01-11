f2f0b88fd200d7497d15a6e3282986280249f80d
Aristex12 <aristex@hotmail.com>
2023-01-11 12:47:42 +0100

    added getters and setters

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -6,10 +6,34 @@ public class Witcher {
 	private int vidas;
 	private boolean hasSoul;
 	
+	public int getVidas() {
+		return vidas;
+	}
+
+	public void setVidas(int vidas) {
+		this.vidas = vidas;
+	}
+
+	public boolean isHasSoul() {
+		return hasSoul;
+	}
+
+	public void setHasSoul(boolean hasSoul) {
+		this.hasSoul = hasSoul;
+	}
+	
 	public static void main(String[] args) {
 		
 		
 
+	}
+
+	private String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
 	}
 
 }
